0d4a92f6f4a4efe592987ce570b0aa50d9998cde
joseph uzuegbu <josephuzuegbu55@gmail.com>
2022-12-08 15:27:39 +0100

    modified:   client/public/index.html         modified:   client/src/components/Layouts/Dashboard.jsx and created   client/src/assets/styles/

--- client/src/components/Layouts/Dashboard.jsx
+++ client/src/components/Layouts/Dashboard.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Outlet, Link } from 'react-router-dom';
-import {Chart as ChartJS, CategoryScale,LinearScale,BarElement, Title,Tooltip,Legend,} from 'chart.js';
+import {Chart as ChartJS, CategoryScale,LinearScale,BarElement, Title,Tooltip,Legend} from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import { faker } from '@faker-js/faker';
+import   '../../assets/styles/dashboard_style.css'
+import  '../../assets/styles/dashboard.css'
 
 ChartJS.register(CategoryScale,LinearScale,BarElement,Title,Tooltip, Legend);
 
@@ -44,66 +46,132 @@ export const data = {
 
 export default function Dashboard() {
   return (
-        <section className='container'>
-          {/* header */}
-              <nav className="navbar">
-                  <Link className="navbar-brand" to="#">
-                    <img src={process.env.PUBLIC_URL + '/imgs/logo/logo.png'} alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
-                    Fitness
+
+   <>
+      <header class="navbar navbar-dark sticky-top bg-success flex-md-nowrap p-0 shadow">
+        <Link className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" to="#">
+          <img src={process.env.PUBLIC_URL + '/imgs/logo/logo.png'} alt="Logo" width="30" height="24" className="d-inline-block me-2 align-text-top img-fluid img-thumbnail"/>
+          Fitness
+        </Link>
+        <button class="navbar-toggler position-absolute d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="true" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <input class="form-control form-control-dark w-100 rounded-0 border-0 bg-body opacity-75" type="text" placeholder="Search" aria-label="Search"/>
+        <div class="navbar-nav">
+          <div class="nav-item text-nowrap">
+            <button class="btn btn-sm btn-danger opacity-75 px-3 m-2" href="#">Sign out</button>
+          </div>
+        </div>
+      </header>
+
+      <div class="container-fluid">
+        <div class="row">
+          <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse show">
+            <div class="position-sticky pt-3 sidebar-sticky">
+              <ul class="nav flex-column">
+                <li class="nav-item">
+                  <Link class="nav-link active" aria-current="page" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home align-text-bottom" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                    Dashboard
                   </Link>
-                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                  </button>
-            </nav>
-        
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file align-text-bottom" aria-hidden="true"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
+                    Orders
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart align-text-bottom" aria-hidden="true"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+                    Products
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users align-text-bottom" aria-hidden="true"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                    Customers
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart-2 align-text-bottom" aria-hidden="true"><line x1="18" y1="20" x2="18" y2="10"></line><line x1="12" y1="20" x2="12" y2="4"></line><line x1="6" y1="20" x2="6" y2="14"></line></svg>
+                    Reports
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers align-text-bottom" aria-hidden="true"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
+                    Integrations
+                  </Link>
+                </li>
+              </ul>
 
-            <section className="d-flex bg-transparent" style={{height:'100vh'}}>
-              {/* sidebar */}
-                <aside className='collapse pe-5' id='navbarSupportedContent'>
-                   aside
-                   <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                      <Link className="nav-link active" aria-current="page" to="#">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="#">Link</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link disabled">Disabled</Link>
-                    </li>
-              
-                    <li className="nav-item">
-                      <Link className="nav-link active" aria-current="page" to="#">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="#">Link</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link disabled">Disabled</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="#">Link</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link disabled">Disabled</Link>
-                    </li>
-                  </ul>
-                </aside>
+              <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+                <span>Saved reports</span>
+                <Link class="link-secondary" href="#" aria-label="Add Link new report">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle align-text-bottom" aria-hidden="true"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
+                </Link>
+              </h6>
+              <ul class="nav flex-column mb-2">
+                <li class="nav-item">
+                  <Link class="nav-link" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text align-text-bottom" aria-hidden="true"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                    Current month
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text align-text-bottom" aria-hidden="true"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                    Last quarter
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text align-text-bottom" aria-hidden="true"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                    Social engagement
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text align-text-bottom" aria-hidden="true"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                    Year-end sale
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
 
-                <main style={{height:'100vh'}}>
-                     <h4 className='lead'>Hello Joseph</h4>
-                     <h6 className=''>Welcome</h6>
-                    {/* <section className='row'> */}
-                      <section className='d-flex flex-column border' style={{position: 'relative', height:'50vh', margin: 'auto', width:"70vw"}}>
-                          <Bar options={options} data={data} width={100}/>
-                      </section>
-                    {/* </section> */}
-                  {/* style={{position: 'relative', height:'40vh', width:"80vw"}} */}
-                     <Outlet/>
-                </main>
-                  
+          <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+              <h1 class="h2">Dashboard</h1>
+               
+              <div class="btn-toolbar mb-2 mb-md-0">
+                <div class="btn-group me-2">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+                </div>
+                <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar align-text-bottom" aria-hidden="true"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+                  This week
+                </button>
+              </div>
+            </div>
+            <section className='row'>
+              <h4 className='lead'>Hello Joseph</h4>
+              <h6 className=''>Welcome</h6>
             </section>
-        </section>
+
+          <section className='row h-75'> 
+                <Bar options={options} data={data} width={100} />
+            </section> 
+            <div className='row'>
+              <Outlet/>
+            </div>
+          </main>
+        </div>
+      </div>
+</>
   )
 }
 
