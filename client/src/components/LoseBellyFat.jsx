4b26d8c9fcbc3f1869e2e1dbf9ca02ebb0d2b66a
unknown <josephuzuegbu55@gmail.com>
2023-01-12 16:40:00 +0100

    trying to my losebellyfat component responsive

--- client/src/components/LoseBellyFat.jsx
+++ client/src/components/LoseBellyFat.jsx
@@ -3,48 +3,44 @@ import '../assets/styles/loseBellyFat.css'
 
 
 export default function LoseBellyFat() {
-    const style = {
-        borderRadius:'1rem'
-    }
   return (
     <section className='lose-belly-fat-wrapper'>
         <div className='row'>
 
             <div className='cards-wrapper'>
-                <div className='card-wrapper' style={style}>
+                <div className='card-wrapper'>
                     <div className='content'>
                         <h6>Name of exercise</h6>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
                     </div>
 
                     <div className='loseBellyFat-header-img-banner'>
-                      <img src={process.env.PUBLIC_URL+'/cartoons/plank-6573171_1280.png'} className='img' alt='' loading='lazy' width='100%' height='100%'/>
+                      <img src={process.env.PUBLIC_URL+'/cartoons/plank-6573171_1280.png'} className='img' alt='' width='100%' height='100%'/>
                     </div>
                     
                 </div>
 
                 <div className='card-wrapper'>
-                      <div className='content'>
+                    <div className='content'>
                       <h6>Name of exercise</h6>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
                     </div>
 
                     <div className='loseBellyFat-header-img-banner'>
-                      <img src={process.env.PUBLIC_URL+'/cartoons/plank-6573171_1280.png'} className='img' alt='' loading='lazy' width='100%' height='100%'/>
+                      <img src={process.env.PUBLIC_URL+'/cartoons/plank-6573171_1280.png'} className='img' alt='' width='100%' height='100%'/>
                     </div>
                     
                 </div>
 
                 <div className='card-wrapper'>
-                      <div className='content'>
-                      <h6>Name of exercise</h6>
+                    <div className='content'>
+                        <h6>Name of exercise</h6>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
                     </div>
 
                     <div className='loseBellyFat-header-img-banner'>
-                      <img src={process.env.PUBLIC_URL+'/cartoons/plank-6573171_1280.png'} className='img' alt='' loading='lazy' width='100%' height='100%'/>
+                      <img src={process.env.PUBLIC_URL+'/cartoons/plank-6573171_1280.png'} className='img' alt='' width='100%' height='100%'/>
                     </div>
-                    
                 </div>
             </div>
 
